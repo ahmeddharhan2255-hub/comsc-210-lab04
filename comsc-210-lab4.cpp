3e0b9cb00d23425a55a19309972f75dbe13f279c
ahmeddharhan2255-hub <ahmeddharhan2255@gmail.com>
2026-09-05 19:55:14 -0700

    Milestone 1

--- comsc-210-lab4.cpp
+++ comsc-210-lab4.cpp
@@ -1,1 +1,27 @@
 // COMSC-210 | Lab 3 | Ahmad Dharhan
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Color 
+{
+    int Red;
+    int Green;
+    int Blue;
+};
+
+int main(){
+
+    Color color1 = {32,45,91};
+    
+    cout << color1.Red << endl;
+
+    cout << color1.Green << endl;
+
+    cout << color1.Blue << endl;
+
+    return 0;
+
+}
